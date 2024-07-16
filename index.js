98582d981d760e13d6e7226afafa531cb3a6c588
Raluca Ghindea <ralucaghindea@yahoo.com>
2024-07-16 22:45:11 +0300

    add: css style; images

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", displayAllProducts);
+const mainContainer = document.querySelector(".main");
+
+function getAllProducts() {
+  const url = "https://668e5802bf9912d4c92de20c.mockapi.io/products";
+  return fetch(url).then((response) => response.json());
+}
+
+function displayAllProducts() {
+  getAllProducts().then(
+    (products) =>
+      (mainContainer.innerHTML = products
+        .map(
+          (product) =>
+            `
+      <div class="product-card flex-col gap-20 items-center justify-between">
+         <h3 class="card-title">${product.name}</h3>
+         <img src=${product.imageUrl} width="150px"/>
+         <p class="card-price">${product.price}</p>
+      </div>
+      `
+        )
+        .join(" "))
+  );
+}
